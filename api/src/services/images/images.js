3fe6c5c29f1c87d2f77a50da07b108d832e9c25f
junkei <junkei@junkeinoMacBook-Pro.local>
2022-07-23 11:28:24 +0900

    first commit

---
+++ api/src/services/images/images.js
@@ -0,0 +1,48 @@
+import { db } from 'src/lib/db'
+import * as Filestack from 'filestack-js'
+
+export const images = () => {
+  return db.image.findMany()
+}
+
+export const image = ({ id }) => {
+  return db.image.findUnique({
+    where: { id },
+  })
+}
+
+export const createImage = ({ input }) => {
+  return db.image.create({
+    data: input,
+  })
+}
+
+export const updateImage = ({ id, input }) => {
+  return db.image.update({
+    data: input,
+    where: { id },
+  })
+}
+
+export const deleteImage = async ({ id }) => {
+  const client = Filestack.init(process.env.REDWOOD_ENV_FILESTACK_API_KEY)
+
+  const image = await db.image.findUnique({ where: { id } })
+
+  // The `security.handle` is the unique part of the Filestack file's url.
+  const handle = image.url.split('/').pop()
+
+  const security = Filestack.getSecurity(
+    {
+      // We set `expiry` at `now() + 5 minutes`.
+      expiry: new Date().getTime() + 5 * 60 * 1000,
+      handle,
+      call: ['remove'],
+    },
+    process.env.REDWOOD_ENV_FILESTACK_SECRET
+  )
+
+  await client.remove(handle, security)
+
+  return db.image.delete({ where: { id } })
+}
---
+++ web/src/pages/MyPagePage/MyPagePage.js
@@ -0,0 +1,25 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const MyPagePage = () => {
+  return (
+    <>
+      <MetaTags title="MyPage" description="MyPage page" />
+
+      <h1>MyPagePage</h1>
+
+      <p>
+        Find me in
+        <code>./web/src/pages/MyPagePage/MyPagePage.js</code>
+      </p>
+
+      <p>
+        My default route is named
+        <code>myPage</code>, link to me with `
+        <Link to={routes.myPage()}>MyPagePage</Link>`
+      </p>
+    </>
+  )
+}
+
+export default MyPagePage
